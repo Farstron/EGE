e022267238c177989c3a3c4a56f67a3ce7178c7b
Farstron <bleskind54@gmail.com>
2025-10-08 17:18:12 +0300

    Закоментировал код

--- task2.py
+++ task2.py
@@ -63,32 +63,32 @@
 
 # F1  =  (x ∨¬ y) → (w ≡ z)
 # F2  =  (x ∨¬ y) ≡ (w → z)
-print("x y w z")
-for x in range(2):
-    for y in range(2):
-        for z in range(2):
-            for w in range(2):
-                f1 = (x or not(y)) <= (w == z)
-                f2 = (x or not(y)) == (w <= z)
-                if f1 == False and f2 == False:
-                    print(x,y,w,z)
-print("___________________")
-print("x y w z")
-for x in range(2):
-    for y in range(2):
-        for z in range(2):
-            for w in range(2):
-                f1 = (x or not(y)) <= (w == z)
-                f2 = (x or not(y)) == (w <= z)
-                if f1 == False:
-                    print(x,y,w,z)
-print("___________________")
-print("x y w z")
-for x in range(2):
-    for y in range(2):
-        for z in range(2):
-            for w in range(2):
-                f1 = (x or not(y)) <= (w == z)
-                f2 = (x or not(y)) == (w <= z)
-                if f2 == False:
-                    print(x,y,w,z)
+# print("x y w z")
+# for x in range(2):
+#     for y in range(2):
+#         for z in range(2):
+#             for w in range(2):
+#                 f1 = (x or not(y)) <= (w == z)
+#                 f2 = (x or not(y)) == (w <= z)
+#                 if f1 == False and f2 == False:
+#                     print(x,y,w,z)
+# print("___________________")
+# print("x y w z")
+# for x in range(2):
+#     for y in range(2):
+#         for z in range(2):
+#             for w in range(2):
+#                 f1 = (x or not(y)) <= (w == z)
+#                 f2 = (x or not(y)) == (w <= z)
+#                 if f1 == False:
+#                     print(x,y,w,z)
+# print("___________________")
+# print("x y w z")
+# for x in range(2):
+#     for y in range(2):
+#         for z in range(2):
+#             for w in range(2):
+#                 f1 = (x or not(y)) <= (w == z)
+#                 f2 = (x or not(y)) == (w <= z)
+#                 if f2 == False:
+#                     print(x,y,w,z)

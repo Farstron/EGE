2d407a38bad588ae4d4a7da9fe691738a9251354
Farstron <bleskind54@gmail.com>
2025-12-17 17:27:00 +0700

    yfsfdhuy

---
+++ 6.py
@@ -0,0 +1,115 @@
+# import turtle as t
+# t.speed(100)
+# t.left(90)
+# l = 50
+# t.up()
+# t.goto(-5*l,-5*l)
+# t.down()
+# for i in range(2):
+#     t.forward(9 * l)
+#     t.right(90)
+#     t.forward(15 * l)
+#     t.right(90)
+# t.up()
+# t.forward(12 * l)
+# t.right(90)
+# t.down()
+# t.color('green')
+# for i in range(2):
+#     t.forward(6 * l)
+#     t.right(90)
+#     t.forward(12 * l)
+#     t.right(90)
+# t.up()
+# for x in range(-5, 11):
+#     for y in range(-5, 11):
+#         t.goto(x * l, y * l)
+#         t.dot(4, 'red')
+# t.done()
+
+
+# import turtle as t
+# t.speed(1000)
+# t.left(90)
+# l = 30
+# for i in range(2):
+#     t.forward(10*l)
+#     t.right(90)
+#     t.forward(18*l)
+#     t.right(90)
+# t.up()
+# t.forward(5*l)
+# t.right(90)
+# t.forward(7*l)
+# t.left(90)
+# t.down()
+# for i in range(2):
+#     t.forward(10*l)
+#     t.right(90)
+#     t.forward(7*l)
+#     t.right(90)
+# t.up()
+# for x in range(0,20):
+#     for y in range(0,20):
+#         t.goto(x*l, y*l)
+#         t.dot(4)
+# t.done()
+
+# import turtle as t
+# t.speed(100)
+# t.left(90)
+# l = 40
+# for i in range(7):
+#     t.forward(10*l)
+#     t.right(120)
+# t.up()
+# for x in range(0, 20):
+#     for y in range(0, 20):
+#         t.goto(x*l, y*l)
+#         t.dot(4)
+# t.done()
+
+
+# import turtle as t
+# t.speed(100)
+# t.left(90)
+# l = 40
+# for i in range(2):
+#     t.forward(3*l)
+#     t.left(90)
+#     t.back(10*l)
+#     t.left(90)
+# t.up()
+# t.back(10*l)
+# t.right(90)
+# t.forward(8*l)
+# t.left(90)
+# t.down()
+# for i in range(2):
+#     t.forward(16*l)
+#     t.right(90)
+#     t.forward(8*l)
+#     t.right(90)
+# t.up()
+# for x in range(0,20):
+#     for y in range(-10,8):
+#         t.goto(x*l, y*l)
+#         t.dot(5)
+# t.done()
+
+
+# import turtle as t
+# t.speed(100)
+# t.left(90)
+# l = 40
+# for i in range(5):
+#     t.forward(9*l)
+#     t.right(90)
+#     t.forward(3*l)
+#     t.right(90)
+# t.up()
+# for x in range(0, 15):
+#     for y in range(0, 15):
+#         t.goto(x*l, y*l)
+#         t.dot(5)
+# t.done()

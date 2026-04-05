925305a8c4a477a939f01ce373a7060288f92d53
Farstron <bleskind54@gmail.com>
2026-04-05 11:37:43 +0700

    asdasdas

--- task24.py
+++ task24.py
@@ -23,30 +23,30 @@
 #     i += 1
 # print(ml)
 
+# https://education.yandex.ru/ege/inf/task/c915cd87-4725-4f62-8f2f-5f5c784267df
+# with open('task24/24 (1).txt') as file:
+#     data = file.read()
+# ml = 0
+# s = ''
+# for i in range(len(data)):
+#     s += data[i]
+#     while s.count('A') > 240:
+#         s = s[1::]
+#     ml = max(ml,len(s)) 
+# print(ml)
 
-with open('task24/24 (1).txt') as file:
+#https://education.yandex.ru/ege/inf/training/24/task/1?examTaskId=083910a9-6d5d-45d9-bfe6-1ae5a008abf2&examTaskNumber=24&taskId=97e2f438-d6ce-4c55-95f5-8384f58faea4&categoryId=34289ff9-4b4d-4d32-8378-b44d98d85090
+with open('task24/24 (2).txt') as file:
     data = file.read()
 
-word = 'ADENXY!'
-
-ml = 0
-i = 0
-l = 0
-k = 0
+nums = '1234567890'
 s = ''
-while i in range(len(data)):
-    if data[i] == word[k]:
+res = []
+for i in range(len(data)):
+    if data[i] in nums:
         s += data[i]
-    elif data[i] == word[k+1]:
-        s += data[i]
-        if k < len(word) - 2:
-            k += 1
-        else:
-            k = 0
     else:
-
-        ml = max(ml,len(s)) 
-        l = 0
-        k = 0
-    i += 1
-print(ml)
+        if s != '':
+            res.append(int(s))
+            s = ''
+print(max(res))
